eb05e79480e446b4f07a6629a58ea9ea05581b61
4NTx <arturrrick@gmail.com>
2023-12-13 03:44:11 -0300

    Usando mail TailWind

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -4,13 +4,13 @@ import styles from './loading.module.css';
 
 export default function Loading() {
     return (
-        <div className={styles.container}>
-            <div className="min-h-screen flex flex-col justify-center items-center">
+        <div className="bg-[#233E47] select-none">
+            <div className="min-h-screen flex flex-col justify-center items-center font-mono">
                 <Image src="/imagens/pikachu.gif" alt="Carregando" width={300} height={300} priority={true} className={styles.pikachu} />
                 <div className={styles.pokebola}>
-                    <Image src="/imagens/pokebola.png" alt="Pokebola" width={150} height={150} priority={true} className={styles.pokebola} />
+                    <Image src="/imagens/pokebola.png" alt="Pokebola" width={160} height={160} priority={true} />
                 </div>
-                <div className={styles.titulo}>
+                <div className="text-[1.5rem] mt-5">
                     Carregando<span className={styles.pontos}>...</span>
                 </div>
             </div>
